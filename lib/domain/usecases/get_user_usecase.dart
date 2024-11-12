e79ea10d476fa4dc8bee1cabde6d459b16ce065e
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-11-12 20:06:08 +0700

    create get user use case

---
+++ lib/domain/usecases/get_user_usecase.dart
@@ -0,0 +1,32 @@
+import 'package:youapp/data/models/user_model.dart';
+import 'package:youapp/domain/repositories/user_repository.dart';
+
+class GetUserDataUseCase {
+  final UserRepository userRepository;
+
+  GetUserDataUseCase(this.userRepository);
+
+  Future<UserModel?> execute() async {
+    try {
+      return await userRepository.fetch();
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<UserModel?> executeLocal() async {
+    try {
+      return await userRepository.fetchFromLocal();
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<UserModel?> executeRemote() async {
+    try {
+      return await userRepository.fetchFromRemote();
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
