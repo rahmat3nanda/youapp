220671fec89de3029f4a1a90adabfb65888cc7a3
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-11-12 20:05:31 +0700

    Create user model

---
+++ lib/data/models/user_model.dart
@@ -0,0 +1,51 @@
+class UserModel {
+  final String? email;
+  final String? username;
+  final String? name;
+  final String? birthday;
+  final String? horoscope;
+  final String? zodiac;
+  final int? height;
+  final int? weight;
+  final List<String>? interests;
+
+  UserModel({
+    this.email,
+    this.username,
+    this.name,
+    this.birthday,
+    this.horoscope,
+    this.zodiac,
+    this.height,
+    this.weight,
+    this.interests,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+        email: json["email"],
+        username: json["username"],
+        name: json["name"],
+        birthday: json["birthday"],
+        horoscope: json["horoscope"],
+        zodiac: json["zodiac"],
+        height: json["height"],
+        weight: json["weight"],
+        interests: json["interests"] == null
+            ? null
+            : List<String>.from(json["interests"]!.map((x) => x)),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "email": email,
+        "username": username,
+        "name": name,
+        "birthday": birthday,
+        "horoscope": horoscope,
+        "zodiac": zodiac,
+        "height": height,
+        "weight": weight,
+        "interests": interests == null
+            ? null
+            : List<dynamic>.from(interests!.map((x) => x)),
+      };
+}
