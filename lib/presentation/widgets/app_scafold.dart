bd76bc9c21afadce8f1157c4ed32b893c10d98d0
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-11-12 20:46:48 +0700

    Create app scaffold

---
+++ lib/presentation/widgets/app_scafold.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class AppScaffold extends StatelessWidget {
+  final PreferredSizeWidget? appBar;
+  final Widget? body;
+  final Widget? floatingActionButton;
+  final FloatingActionButtonLocation? floatingActionButtonLocation;
+  final Widget? bottomNavigationBar;
+  final Widget? bottomSheet;
+
+  const AppScaffold({
+    super.key,
+    this.appBar,
+    this.body,
+    this.floatingActionButton,
+    this.floatingActionButtonLocation,
+    this.bottomNavigationBar,
+    this.bottomSheet,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar,
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Color(0xFF1F4247),
+              Color(0xFF0D1D23),
+              Color(0xFF09141A),
+            ],
+          ),
+        ),
+        child: body,
+      ),
+      floatingActionButton: floatingActionButton,
+      floatingActionButtonLocation: floatingActionButtonLocation,
+      bottomNavigationBar: bottomNavigationBar,
+      bottomSheet: bottomSheet,
+      backgroundColor: Colors.transparent,
+    );
+  }
+}
